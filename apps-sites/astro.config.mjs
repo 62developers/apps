222021582bbe8149929b2f73b7813d0c505ccbbf
masumar <188675200+umarrudyanto@users.noreply.github.com>
2024-12-17 02:16:00 +0000

    learn

---
+++ apps-sites/astro.config.mjs
@@ -0,0 +1,15 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import mdx from '@astrojs/mdx';
+
+import sitemap from '@astrojs/sitemap';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), mdx(), sitemap(), tailwind()],
+});
